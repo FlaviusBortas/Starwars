df2282b17d10ea6e5f1e70b7e5f655a9222ad243
Flavius Bortas <flavius.bortas@detroitlabs.com>
2018-10-29 14:29:57 -0400

    added character.sw to models folder and created Character Class

---
+++ Assessment/Assessment/Model/Character.swift
@@ -0,0 +1,13 @@
+//
+//  Character.swift
+//  Assessment
+//
+//  Created by Flavius Bortas on 10/29/18.
+//  Copyright © 2018 Flavius Bortas. All rights reserved.
+//
+
+import Foundation
+
+class Character {
+    
+}
