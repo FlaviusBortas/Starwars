13795920a0dc5d4e2928ff4c93a6d092419eb7f4
Flavius Bortas <flavius.bortas@detroitlabs.com>
2018-10-30 11:21:26 -0400

    Moved load details func from CharactersTVC to a new file

--- Assessment/Assessment/Networking/NetworkManager.swift
+++ Assessment/Assessment/Networking/NetworkManager.swift
@@ -7,3 +7,36 @@
 //
 
 import Foundation
+
+
+class NetworkManager {
+    
+    func loadData<T: Decodable>(from urlString: String, completion: @escaping (T) -> ()) {
+        // Create a configuration
+        let configuration = URLSessionConfiguration.ephemeral
+        
+        // Create a session
+        let session = URLSession(configuration: configuration)
+        
+        // Setup the url
+        let url = URL(string: urlString)!
+        
+        // Create the task
+        let task = session.dataTask(with: url) { (data, response, error) in
+            
+            guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200, let data = data else {
+                return
+            }
+            do {
+                let decoder = JSONDecoder()
+                let object = try decoder.decode(T.self, from: data)
+                
+                completion(object)
+                
+            } catch {
+                print("Error info: \(error)")
+            }
+        }
+        task.resume()
+    }
+}
