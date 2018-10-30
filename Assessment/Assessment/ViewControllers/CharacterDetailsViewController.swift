3c1eefbd0db7a7cb636871b0a8ae4cd7253f13f6
Flavius Bortas <flavius.bortas@detroitlabs.com>
2018-10-30 11:23:07 -0400

    removed load func and put it in seperate file, cleadned up useless code

--- Assessment/Assessment/ViewControllers/CharacterDetailsViewController.swift
+++ Assessment/Assessment/ViewControllers/CharacterDetailsViewController.swift
@@ -10,8 +10,36 @@ import UIKit
 
 class CharacterDetailsViewController: UIViewController {
     
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var birthYearLabel: UILabel!
+    @IBOutlet weak var genderLabel: UILabel!
+    @IBOutlet weak var homeWorldLabel: UILabel!
+    @IBOutlet weak var speciesLabel: UILabel!
+    
+    var manager = NetworkManager()
+    var character: Character?
+    let queue = OperationQueue.main
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        guard let character = character else {
+            print("No Character")
+            return
+        }
+        
+        manager.loadData(from: character.homeWorldURL) { (homeWorld: HomeWorld) in
+            
+            self.queue.addOperation {
+                self.homeWorldLabel.text = homeWorld.name
+            }
+        }
+        
+        manager.loadData(from: character.speciesURL.first!) { (species: Species) in
+            
+            self.queue.addOperation {
+                self.speciesLabel.text = species.name
+            }
+        }
     }
 }
