3694659d041f9e33b0f40384efdbe3e87b823b23
Flavius Bortas <flavius.bortas@detroitlabs.com>
2018-10-30 10:23:33 -0400

    added a new VC.swift file

--- Assessment/Assessment/ViewControllers/CharacterDetailsViewController.swift
+++ Assessment/Assessment/ViewControllers/CharacterDetailsViewController.swift
@@ -6,4 +6,12 @@
 //  Copyright © 2018 Flavius Bortas. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class CharacterDetailsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
