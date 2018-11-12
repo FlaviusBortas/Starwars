5f582e45d01c45017fb3cc87141dd7162ea24f47
Flavius <flav@Flaviuss-MacBook-Pro.local>
2018-11-12 17:22:01 -0500

    commit

--- Assessment/Assessment/Networking/NetworkManager.swift
+++ Assessment/Assessment/Networking/NetworkManager.swift
@@ -25,7 +25,6 @@ class NetworkManager {
             
             guard let httpResponse = response as? HTTPURLResponse else {
                 print("status code or data failed")
-                print("added a print")
                 return
             }
             
